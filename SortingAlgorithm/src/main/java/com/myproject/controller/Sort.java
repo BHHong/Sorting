7b43e34f5628c975e57eda3f7bf5a998d322594f
BHH <BHH@192.168.0.5>
2017-04-19 14:31:00 +0100

    Insertion, Selection, Bubble

--- SortingAlgorithm/src/main/java/com/myproject/controller/Sort.java
+++ SortingAlgorithm/src/main/java/com/myproject/controller/Sort.java
@@ -4,40 +4,28 @@ import java.util.Arrays;
 
 public class Sort {
 
-	public static void bubbleSort(Comparable array[]) {
-		System.out.println("Before Bubble Sort");
-		System.out.println(Arrays.toString(array));
-		System.out.println("Start Bubble Sort");
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 1; j < array.length; j++) {
-				if (isLess(array[j], array[j - 1])) {
-					swap(array, j, j - 1);
-					System.out.println(Arrays.toString(array));
-				}
-			}
-		}
-		System.out.println("After Bubble Sort");
-		System.out.println(Arrays.toString(array));
-	}
+	static int count1 = 0;
+	static int count2 = 0;
+	static int count3 = 0;
 
 	public static void insertionSort(Comparable array[]) {
 		System.out.println("Before Insertion Sort");
 		System.out.println(Arrays.toString(array));
 		System.out.println("Start Insertion Sort");
 		for (int i = 1; i < array.length; i++) {
-			Comparable numToSort = array[i];
+			Comparable valueToSort = array[i];
 			int targetIndex = i;
-			while (targetIndex > 0 && isLess(numToSort, array[targetIndex - 1])) {
+			while (targetIndex > 0 && isLess(valueToSort, array[targetIndex - 1])) {
 				array[targetIndex] = array[targetIndex - 1];
 				targetIndex--;
+				count1++;
 			}
-			array[targetIndex] = numToSort;
+			array[targetIndex] = valueToSort;
 			System.out.println(Arrays.toString(array));
 		}
-		System.out.println("After Insertion Sort");
-		System.out.println(Arrays.toString(array));
+		System.out.println("Total loop: " + count1);
 	}
-	
+
 	public static void selectionSort(Comparable array[]) {
 		System.out.println("Before Selection Sort");
 		System.out.println(Arrays.toString(array));
@@ -48,12 +36,28 @@ public class Sort {
 				if (isLess(array[j], array[minIndex])) {
 					minIndex = j;
 				}
+				count2++;
 			}
-			swap(array, i , minIndex);
+			swap(array, i, minIndex);
 			System.out.println(Arrays.toString(array));
 		}
-		System.out.println("After Selection Sort");
+		System.out.println("Total loop: " + count2);
+	}
+
+	public static void bubbleSort(Comparable array[]) {
+		System.out.println("Before Bubble Sort");
 		System.out.println(Arrays.toString(array));
+		System.out.println("Start Bubble Sort");
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 1; j < array.length - i; j++) {
+				if (isLess(array[j], array[j - 1])) {
+					swap(array, j, j - 1);
+					System.out.println(Arrays.toString(array));
+				}
+				count3++;
+			}
+		}
+		System.out.println("Total loop: " + count3);
 	}
 
 	private static boolean isLess(Comparable j, Comparable k) {
--- SortingAlgorithm/src/main/java/com/myproject/view/Main.java
+++ SortingAlgorithm/src/main/java/com/myproject/view/Main.java
@@ -8,13 +8,13 @@ import com.myproject.controller.Sort;
 public class Main {
 
 	public static void main(String[] args) {
-		Character[] charArray = {'a','b','c','x','y','z','2','1','3'};
+		Character[] charArray = {'a','b','c','x','y','z','1','2','3'};
 		Integer[] integerArray = {1,2,3,4,5,6,7,8,9,10};
 		Double[] doubleArray = {1.11,2.22,3.33,4.44,5.55,6.66};
 		String[] stringArray = {"Jacky", "Aaron", "James", "Queen", "Keele", "Baron"};
 		
 		// change for array with different data type
-		Comparable[] arr = charArray;
+		Comparable[] arr = integerArray;
 		
 		System.out.println();
 		Collections.shuffle(Arrays.asList(arr));
